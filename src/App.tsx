c05ca956f780a5f5120eb0598b392fdfa4c0affd
Maxime.G <gilbert.maxime01@gmail.com>
2024-10-07 17:08:27 +0200

    Commit Init
    commit initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container-max">
+      <div className="image-container">
+        <div className="animated-text">
+          F1 <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/index-carousel/carousel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function carousel() {
+    
+}
+
+export default carousel
