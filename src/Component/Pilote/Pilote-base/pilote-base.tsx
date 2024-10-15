4943c8dced6064e52f64d25f5696754155af62b6
MaxDev <86471587+MaximeG44270@users.noreply.github.com>
2024-10-15 17:03:20 +0200

    Commit 6
    ajout pilote et suppression pour base

---
+++ src/Component/Pilote/Pilote-base/pilote-base.tsx
@@ -0,0 +1,54 @@
+// src/Component/Pilote/pilote-base.tsx
+
+import React, { useEffect, useState } from 'react';
+import './pilote-base.css';
+
+interface Driver {
+  firstName: string;
+  lastName: string;
+  teamColor: string;
+  countryOfOrigin: string;
+  teamName: string;
+  number: string;
+  profilPicture: string;
+}
+
+const PiloteBase: React.FC = () => {
+  const [drivers, setDrivers] = useState<Driver[]>([]);
+
+  useEffect(() => {
+    fetch('/json/pilote.json')
+      .then(response => response.json())
+      .then(data => setDrivers(data.drivers))
+      .catch(error => console.error('Error fetching the data:', error));
+  }, []);
+
+  return (
+    <div className="pilote-list">
+      {drivers.map(driver => (
+        <div key={driver.number} className="body-card-pilote">
+          <div className='container-pilote-top'>
+            <div className="container-color-name">
+              <div className="container-color-team-pilote" style={{ backgroundColor: driver.teamColor }}></div>
+              <div className="name-pilote">
+                <div className="first-name-pilote">{driver.firstName}</div>
+                <div className="last-name-pilote">{driver.lastName}</div>
+              </div>
+            </div>
+            <img className="origin-pays" src={driver.countryOfOrigin} />
+          </div>
+          <div className="border-inter-ligne"></div>
+          <div className="container-info-pilote">
+            <div className="name-team-pilote">{driver.teamName}</div>
+            <div className="container-number-picture">
+              <div className="number-pilote">{driver.number}</div>
+              <img className="picture-pilote" src={driver.profilPicture} alt={`${driver.firstName} ${driver.lastName}`} />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PiloteBase;
--- src/Pages/Pilote/Pilote.tsx
+++ src/Pages/Pilote/Pilote.tsx
@@ -1,28 +1,7 @@
-import React from "react";
-import './Pilote.css';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import PiloteAlbon from "../../Component/Pilote/Albon/Albon";
-import PiloteAlanso from "../../Component/Pilote/Alanso/Alanso";
-import PiloteBearman from "../../Component/Pilote/Bearman/Bearman";
-import PiloteBottas from "../../Component/Pilote/Bottas/Bottas";
-import PiloteColapinto from "../../Component/Pilote/Colapinto/Colapinto";
-import PiloteGasly from "../../Component/Pilote/Gasly/Gasly";
-import PiloteHamilton from "../../Component/Pilote/Hamilton/Hamilton";
-import PiloteHulkenberg from "../../Component/Pilote/Hulkenberg/Hulkenberg";
-import PiloteLeclerc from "../../Component/Pilote/Leclerc/Leclerc";
-import PiloteMagnussen from "../../Component/Pilote/Magnussen/Magnussen";
-import PiloteNorris from "../../Component/Pilote/Norris/Norris";
-import PiloteOcon from "../../Component/Pilote/Ocon/Ocon";
-import PilotePerez from "../../Component/Pilote/Perez/Perez";
-import PilotePiastri from "../../Component/Pilote/Piastri/Piastri";
-import PiloteRicciardo from "../../Component/Pilote/Ricciardo/Ricciardo";
-import PiloteRussell from "../../Component/Pilote/Russell/Russell";
-import PiloteSainz from "../../Component/Pilote/Sainz/Sainz";
-import PiloteSargeant from "../../Component/Pilote/Sargeant/Sargeant";
-import PiloteStroll from "../../Component/Pilote/Stroll/Stroll";
-import PiloteTsunoda from "../../Component/Pilote/Tsunoda/Tsunoda";
-import PiloteVerstappen from "../../Component/Pilote/Verstappen/Verstappen";
-import PiloteZhou from "../../Component/Pilote/Zhou/Zhou";
+import './Pilote.css';
+import PiloteBase from '../../Component/Pilote/Pilote-base/pilote-base';
 
 const Pilote: React.FC = () => {
     const navigate = useNavigate();
@@ -30,42 +9,21 @@ const Pilote: React.FC = () => {
     const handleNavigate = (path: string) => {
         navigate(path);
     };
-
     return (
         <div className="bg">
             <div className="nav">
                 <ul className="nav-list">
                     <li onClick={() => handleNavigate('/circuit')}>Circuit</li>
                     <li onClick={() => handleNavigate('/team')}>Équipes</li>
-                    <li onClick={() => handleNavigate('/pilotes')}>Pilotes</li>
+                    <li onClick={() => handleNavigate('/pilote')}>Pilotes</li>
                     <li onClick={() => handleNavigate('/onboard')}>On Board</li>
                 </ul>
             </div>
-            <h1>Bienvenue sur la page Pilote</h1>
-            <PiloteAlbon />
-            <PiloteAlanso />
-            <PiloteBearman />
-            <PiloteBottas />
-            <PiloteColapinto />
-            <PiloteGasly />
-            <PiloteHamilton />
-            <PiloteHulkenberg />
-            <PiloteLeclerc />
-            <PiloteMagnussen />
-            <PiloteNorris />
-            <PiloteOcon />
-            <PilotePerez />
-            <PilotePiastri />
-            <PiloteRicciardo />
-            <PiloteRussell />
-            <PiloteSainz />
-            <PiloteSargeant />
-            <PiloteStroll />
-            <PiloteTsunoda />
-            <PiloteVerstappen />
-            <PiloteZhou />
+            <div className="team-component">
+                <PiloteBase />
+            </div>
         </div>
     );
-}
+};
 
 export default Pilote;
